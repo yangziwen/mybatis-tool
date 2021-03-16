07bed886f4b7b16cb844099cebbeffa17efb1979
yangziwen <yangziwen@meituan.com>
2021-03-16 17:47:19 +0800

    添加一个增加简单注释的generator

---
+++ src/main/java/io/github/yangziwen/mybatistool/generator/SimpleCommentGenerator.java
@@ -0,0 +1,25 @@
+package io.github.yangziwen.mybatistool.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+public class SimpleCommentGenerator extends DefaultCommentGenerator {
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine(" * " + introspectedTable.getRemarks());
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        field.addJavaDocLine("/**");
+        field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+        field.addJavaDocLine(" */");
+    }
+
+}
